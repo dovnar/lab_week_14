5f89ee48e9d1fd11c5e33388c7ce0f80ed049e22
setlol <dovnar.maxim@gmail.com>
2016-09-18 15:33:14 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+void printArray(int* arr, int size);
+void compositionEndSumArray(int* arr, int const* size, int* sum, int* composition);
+int main()
+{
+	int sum = 0;
+	int composition = 1;
+	int const size{ 10 };
+	int arr[size]{ 2,6,4,12,5,7,9,11,3,8 };
+	printArray(arr, size);
+	compositionEndSumArray(arr, &size, &sum, &composition);
+	cout << "sum array = " << sum << endl << "composition array = " << composition << endl;
+}
+
+void printArray(int * arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(arr + i) << " ";
+	}
+	cout << endl;
+}
+
+void compositionEndSumArray(int * arr, int const * size, int * sum, int * composition)
+{
+	for (int i = 0; i < *size; i++)
+	{
+		*sum += *(arr + i);
+		*composition *= *(arr + i);
+	}
+}
+
+
+
+
+
+
+
+/*Написать функцию, которая получает указатель на массив и его размер,
+и возвращает сумму и произведение его элементов в двух параметрах-указателях.*/
---
+++ task2.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+using namespace std;
+int distributesArray(int* arr, int const* size);
+void printArray(int* arr, int size);
+int main()
+{
+	int const size{ 20 };
+	int arr[size]{ -12,0,-1,15,0,16,-5,-4,3,0,5,-15,0,2,0,-2,-8,8,4,0 };
+	printArray(arr, size);
+	*arr = distributesArray(arr, &size);
+	for (int i = 0; i < 3; i++)
+	{
+		if (i == 0)
+		{
+			cout << "count plus number = " << arr[i] << endl;
+		}
+		else if (i == 1)
+		{
+			cout << "count minus number = " << arr[i] << endl;
+		}
+		else
+		{
+			cout << "count null number = " << arr[i] << endl;
+		}
+	}
+}
+
+int distributesArray(int * arr, int const* size)
+{
+	int countNull = 0;
+	int countPlus = 0;
+	int countMinus = 0;
+	for (int i = 0; i < *size; i++)
+	{
+		if (*(arr + i) > 0)
+		{
+			countPlus++;
+		}
+		else if (*(arr + i) < 0)
+		{
+			countMinus++;
+		}
+		else
+		{
+			countNull++;
+		}
+	}
+	*arr = countPlus;
+	*(arr + 1) = countMinus;
+	*(arr + 2) = countNull;
+	return *arr;
+}
+
+void printArray(int * arr, int size)
+{
+	
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(arr + i) << " ";
+	}
+	cout << endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*Написать функцию, которая получает указатель на массив и его размер, 
+и возвращает количество отрицательных, положительных и нулевых элементов массива.*/
